cea8b63dc7a1dd9e367b7790123f995ef3654842
Andre <35333275+Blackfiree@users.noreply.github.com>
2020-02-02 13:55:34 +0100

    added post api for logbook and requests

--- backend/api/v3/logbooks.py
+++ backend/api/v3/logbooks.py
@@ -1,6 +1,9 @@
 from django.http import JsonResponse, HttpResponse
 import json
-from cms.models import Logbook, Region
+from cms.models import Logbook, Region , Vehicle
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render, redirect, get_object_or_404
+
 
 def transform_logbook(logbook: Logbook):
     return {
@@ -15,10 +18,25 @@ def transform_logbook(logbook: Logbook):
         'comments': logbook.comments,
         'finished': logbook.finished
     }
-
 def logbooks(request):
     _logbooks = Logbook.objects.all()
     result = []
     for logbook in _logbooks:
         result.append(transform_logbook(logbook))
     return JsonResponse(result, safe=False)  # Turn off Safe-Mode to allow serializing arrays
+
+@csrf_exempt
+def newlogbook(request):
+    if request.method != 'POST':
+        return HttpResponse(f'Invalid request method.', status=405)
+
+    data = json.loads(request.body)
+    data = data.get("data")
+    data = json.loads(data)
+    try:
+        vehicle = get_object_or_404(Vehicle, id=data["vehicle"])
+        logbook = Logbook(vehicle = vehicle,driver=data["driver"],coDriver=data["coDriver"],startMileage=int(data["startMileage"]),press= data["press"])
+        logbook.save()
+        return HttpResponse(status=200)
+    except ValueError:
+        return HttpResponse("Bad request.", content_type='text/plain', status=400)
--- backend/api/v3/requests.py
+++ backend/api/v3/requests.py
@@ -1,7 +1,8 @@
-from django.http import JsonResponse
-
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
 from cms.models import Request, Region
-
+import json
 def transform_request(request: Request):
     if request.assignedBus is None:
         return {
@@ -63,3 +64,23 @@ def requests(request):
     for request in _requests:
         result.append(transform_request(request))
     return JsonResponse(result, safe=False)  # Turn off Safe-Mode to allow serializing arrays
+
+@csrf_exempt
+def newrequest(request):
+    if request.method != 'POST':
+        return HttpResponse(f'Invalid request method.', status=405)
+
+    data = json.loads(request.body)
+    data = data.get("data")
+    data = json.loads(data)
+    try:
+        region = get_object_or_404(Region, name=data["region"])
+        request = Request(pinname =data.get["pinname"],wheelchair = data.get["wheelchair"],gender = data.get["gender"]
+         ,medicalNeeds= data.get["medicalNeeds"],luggage = data.get["luggage"], isoMat= data.get["isoMat"], blanket= data.get["blanket"],jacket = data.get["jacket"]
+         ,sleepingBag = data.get["sleepingBag"],children = data.get["children"],pets = data.get["pets"],group = data.get["group"],helpername = data.get["helpername"]
+         ,phone = data.get["phone"],region = region,address = data.get["address"],postcode = data.get["postcode"],latitude = data.get["latitude"]
+         ,longitude = data.get["longitude"])
+        request.save()
+        return HttpResponse(status=200)
+    except ValueError:
+        return HttpResponse("Bad request.", content_type='text/plain', status=400)
