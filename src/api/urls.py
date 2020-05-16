4d21940bc0785ec4d51a6efc53621e7052f17f22
Timo Ludwig <ludwig+secure@integreat-app.de>
2020-05-16 16:06:38 +0200

    Add pages endpoints to APIv3

--- src/api/urls.py
+++ src/api/urls.py
@@ -6,11 +6,16 @@ from django.conf.urls import include, url
 from .v3.languages import languages
 from .v3.regions import regions
 from .v3.accommodations import accommodations
+from .v3.pages import pages
+from .v3.single_page import single_page
+
 
 urlpatterns = [
     url(r'regions/$', regions, name='regions'),
     url(r'(?P<region_slug>[-\w]+)/', include([
         url(r'languages/$', languages),
-        url(r'(?P<language_code>[-\w]+)/accommodations/$', accommodations)
+        url(r'(?P<language_code>[-\w]+)/accommodations/$', accommodations),
+        url(r'(?P<language_code>[-\w]+)/pages/$', pages),
+        url(r'(?P<language_code>[-\w]+)/page/$', single_page),
     ])),
 ]
